a7170b16c38f7783795fe0cd141daa12c1cdf7c7
ACatActor <1024906432@qq.com>
2022-08-01 13:13:29 +0800

    MVP变换

--- shader_s.h
+++ shader_s.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef SHADER_H
 #define SHADER_H
-
+#include "glm/glm.hpp"
 #include <glad/glad.h>
 
 #include <string>
@@ -27,6 +27,20 @@ public:
     void setInt(const std::string& name, int value) const;
     // ------------------------------------------------------------------------
     void setFloat(const std::string& name, float value) const;
+    void setVec2(const std::string& name, const glm::vec2& value) const;
+    void setVec2(const std::string& name, float x, float y) const;
+    // ------------------------------------------------------------------------
+    void setVec3(const std::string& name, const glm::vec3& value) const;
+    void setVec3(const std::string& name, float x, float y, float z) const;
+    // ------------------------------------------------------------------------
+    void setVec4(const std::string& name, const glm::vec4& value) const;
+    void setVec4(const std::string& name, float x, float y, float z, float w) const;
+    // ------------------------------------------------------------------------
+    void setMat2(const std::string& name, const glm::mat2& mat) const;
+    // ------------------------------------------------------------------------
+    void setMat3(const std::string& name, const glm::mat3& mat) const;
+    // ------------------------------------------------------------------------
+    void setMat4(const std::string& name, const glm::mat4& mat) const;
 
 private:
     // utility function for checking shader compilation/linking errors.
